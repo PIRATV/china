4b51a0d1ae5e45e3a9d8a17686857b964a736e04
PIRATV <piratv@inbox.ru>
2014-05-29 13:44:35 +0400

    links css

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -5,4 +5,9 @@ module SiteHelper
   def user_status_image user
     image_tag image_path("user_#{user.online? ? 'on' : 'off'}line.gif")
   end
+  def check_active_page action
+    action.downcase!
+    return ' class=active' if action_name.downcase == action or request.fullpath =~ /\b#{action}\b/ or URI.decode_www_form_component(request.fullpath) =~ /\b#{action}\b/
+    nil
+  end
 end
